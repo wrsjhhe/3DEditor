4000466451189e5f09e3fb4829028419b1370113
wrsjhhe <365843679@qq.com>
2017-07-26 22:57:19 +0800

    ？？

--- js/Project.js
+++ js/Project.js
@@ -76,6 +76,7 @@ Project.windowResized = function() {
     Project.renderer.setSize( document.getElementById("viewport").clientWidth,document.getElementById("viewport").clientHeight);
 
     Project.render();
+
 };
 
 Project.getSelected = function (mouse) {
